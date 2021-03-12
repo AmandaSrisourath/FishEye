5c5f9178bec426366aad877439460f710e0338c2
Amandozz <amanda.srisou@gmail.com>
2021-03-12 17:37:57 +0100

    ad photographer-nav in js

--- public/photographer-detail.js
+++ public/photographer-detail.js
@@ -71,13 +71,31 @@ console.log(id);
 
 const photographers = _data_json__WEBPACK_IMPORTED_MODULE_0__.photographers;
 
-const foundPhotographerId = photographers.find(photographer => photographer.id === parseInt(id, 10));
-console.log(foundPhotographerId);
+const foundPhotographer = photographers.find(photographer => photographer.id === parseInt(id, 10));
+console.log(foundPhotographer);
 
+const photographerName = document.querySelector('#photographer-name');
+photographerName.innerHTML = foundPhotographer.name;
 
+const photographerLocation = document.querySelector('.location');
+photographerLocation.innerHTML = `${foundPhotographer.city}, ${foundPhotographer.country}`;
 
+const photographerSlogan = document.querySelector('.slogan');
+photographerSlogan.innerHTML = foundPhotographer.tagline;
 
+foundPhotographer.tags.forEach((tag) => {
+    const photographerTags = document.querySelector('#photographer-tags');
+    const link = document.createElement('a');
+    link.classList.add("tag");
+    link.innerHTML = `#${tag}`
+    photographerTags.appendChild(link);
+});
 
+const photographerPortrait = document.querySelector('.profile-picture');
+photographerPortrait.src = `./portraitPicture/${foundPhotographer.portrait}`;
+
+const photographerPrice = document.querySelector('#price');
+photographerPrice.innerHTML = foundPhotographer.price;
 }();
 /******/ })()
 ;
--- src/photographer-detail.js
+++ src/photographer-detail.js
@@ -11,9 +11,28 @@ console.log(id);
 
 const photographers = data.photographers;
 
-const foundPhotographerId = photographers.find(photographer => photographer.id === parseInt(id, 10));
-console.log(foundPhotographerId);
+const foundPhotographer = photographers.find(photographer => photographer.id === parseInt(id, 10));
+console.log(foundPhotographer);
 
+const photographerName = document.querySelector('#photographer-name');
+photographerName.innerHTML = foundPhotographer.name;
 
+const photographerLocation = document.querySelector('.location');
+photographerLocation.innerHTML = `${foundPhotographer.city}, ${foundPhotographer.country}`;
 
+const photographerSlogan = document.querySelector('.slogan');
+photographerSlogan.innerHTML = foundPhotographer.tagline;
 
+foundPhotographer.tags.forEach((tag) => {
+    const photographerTags = document.querySelector('#photographer-tags');
+    const link = document.createElement('a');
+    link.classList.add("tag");
+    link.innerHTML = `#${tag}`
+    photographerTags.appendChild(link);
+});
+
+const photographerPortrait = document.querySelector('.profile-picture');
+photographerPortrait.src = `./portraitPicture/${foundPhotographer.portrait}`;
+
+const photographerPrice = document.querySelector('#price');
+photographerPrice.innerHTML = foundPhotographer.price;
